fae2a40181b9686b98dd8d5e826740f0b8a1d02e
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-11-30 03:25:56 +0200

    initial commit

---
+++ app/src/main/java/eg/edu/guc/cirkit/ActionBarMenuActivity.java
@@ -0,0 +1,11 @@
+package eg.edu.guc.cirkit;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Base class for any activity that wants the default menu items
+ * Created by ahi on 11/30/16.
+ */
+
+public class ActionBarMenuActivity extends AppCompatActivity {
+}
